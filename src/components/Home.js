124447e651030622c7b3ff518106de9b626e7576
Luke Gibson <luke.gibson98@hotmail.com>
2021-11-15 23:39:51 +0000

    banner v1

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,16 @@ import Segment from './Segment';
 const Home = () => {
   return (
     <div className="home">
+      <div className="banner">
+        <div className="banner-container">
+          <div className="banner-image">
+            <img src="/images/bsc-logo.png" alt="banner-imagee" />
+          </div>
+          <h1 className="banner-text">
+            Brighton<br/>Squash<br/>Club
+          </h1>
+        </div>
+      </div>
       <Segment segmentIndex={0} title='Brighton Squash Club' />
       <Segment segmentIndex={1} title='About Us' />
       <Segment segmentIndex={2} title='On Court Action' />
